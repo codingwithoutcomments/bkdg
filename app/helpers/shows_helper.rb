a7a299bf51e4845211b9f682872197fcac274f69
Chris Reath <Chris.Reath@gmail.com>
2010-02-01 08:43:56 -0800

    added when comments were created

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -55,6 +55,32 @@ module ShowsHelper
     return output;
   end
   
+  def didwhen(old_time)
+
+    val = Time.now - old_time
+    #puts val
+    if val < 10 then
+      result = 'just a moment ago'
+    elsif val < 40  then
+      result = 'less than ' + (val * 1.5).to_i.to_s.slice(0,1) + '0 seconds ago'
+    elsif val < 60 then
+      result = 'less than a minute ago'
+    elsif val < 60 * 1.3  then
+      result = "1 minute ago"
+    elsif val < 60 * 50  then
+      result = "#{(val / 60).to_i} minutes ago"
+    elsif val < 60  * 60  * 1.4 then
+      result = 'about 1 hour ago'
+    elsif val < 60  * 60 * (24 / 1.02) then
+      result = "about #{(val / 60 / 60 * 1.02).to_i} hours ago"
+    else
+      result = old_time.strftime("%H:%M %p %B %d, %Y")
+
+    end
+    result
+  end
+  
+  
   def attending_highlight_helper(user_logged_in,shows_attending, show)
     if(user_logged_in)
         shows_attending.each do |show_attending|
