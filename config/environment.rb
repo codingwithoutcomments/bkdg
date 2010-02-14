b50c0c8f7433dc0fd5cd1bdfd5dc4a2e25825cbd
Chris Reath <Chris.Reath@gmail.com>
2010-02-13 18:46:38 -0800

    heroku deployment

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 #uncomment this line for deployment to heroku
-#equire "#{RAILS_ROOT}/vendor/gems/environment.rb"
+require "#{RAILS_ROOT}/vendor/gems/environment.rb"
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
