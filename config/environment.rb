b80b0d9acdd606987a85c501c8c5dd1b5c95e885
Chris Reath <Chris.Reath@gmail.com>
2010-02-19 09:17:14 -0800

    test

--- config/environment.rb
+++ config/environment.rb
@@ -8,10 +8,10 @@
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
-require File.join(File.dirname(__FILE__), 'boot')
+#require File.join(File.dirname(__FILE__), 'boot')
 
 #uncomment this line for deployment to heroku
-require "#{RAILS_ROOT}/vendor/gems/environment.rb"
+#require "#{RAILS_ROOT}/vendor/gems/environment.rb"
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
