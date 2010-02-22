7bf0c565895be18812603da10b5e5187f0486d0a
Chris Reath <Chris.Reath@gmail.com>
2010-02-21 22:23:19 -0800

    work on getting mail to work 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = true
+config.action_mailer.raise_delivery_errors = false
 #ActionMailer::Base.smtp_settings = {
 #  :address => "smtp.sendgrid.net",
 #  :port => '25',
@@ -23,13 +23,13 @@ config.action_mailer.raise_delivery_errors = true
 #  :user_name => "Chris.Reath@badkidsdancegood.com",
 #  :password => "xftmmgk7"
 #}
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
+#ActionMailer::Base.delivery_method = :smtp
+#ActionMailer::Base.smtp_settings = {
 
-  :address => 'smtp.sendgrid.net',
-  :port => 25,
-  :domain => 'badkidsdancegood.com',
-  :authentication => :plain,
-  :user_name => 'Chris.Reath@badkidsdancegood.com',
-  :password => 'xftmmgk7'
-}
+#  :address => 'smtp.sendgrid.net',
+#  :port => 25,
+#  :domain => 'badkidsdancegood.com',
+#  :authentication => :plain,
+#  :user_name => 'Chris.Reath@badkidsdancegood.com',
+#  :password => 'xftmmgk7'
+#}
