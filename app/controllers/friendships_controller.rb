37cc156a6b7faf5706f461dbd2923c2fa9a37cc0
Chris Reath <Chris.Reath@gmail.com>
2010-04-02 08:06:29 -0700

    more work towards friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,15 +3,13 @@ class FriendshipsController < ApplicationController
   # POST /friendships
   # POST /friendships.xml
   def create
+    @user = User.find(params[:friend_id])
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-
-      if @friendship.save
-        flash[:notice] = 'Added Friend'
-        redirect_to root_url
-      else
-        flash[:notice] = 'Unable To Add Friend'
-        redirect_to root_url
-      end
+    @friendship.save
+   
+    respond_to do |format|
+        format.js
+     end
   end
 
   # DELETE /friendships/1
@@ -19,8 +17,10 @@ class FriendshipsController < ApplicationController
   def destroy
     @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
-    flash[:notice] = "Removed Friendship."
-    redirect_to current_user
-
+  
+    respond_to do |format|
+      format.js
+    end
   end
+
 end
