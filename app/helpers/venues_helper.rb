45fb85582386fb9472d892887747225ad55e6661
Chris Reath <Chris.Reath@gmail.com>
2010-05-10 20:30:48 -0700

    fixed venue page mistake

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,8 +1,11 @@
 module VenuesHelper
   
   def verify_has_http(venueWebsite)
-    if(venueWebsite.at(0) != "h" && venueWebsite.at(2) != "p") then 
-      return "http://" + venueWebsite
+    debugger()
+    if(venueWebsite != "") then
+      if(venueWebsite.at(0) != "h" && venueWebsite.at(2) != "p") then 
+        return "http://" + venueWebsite
+      end
     end
     
     return venueWebsite
